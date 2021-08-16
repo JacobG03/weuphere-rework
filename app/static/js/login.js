94188c837255e1fae61eb4408191ec00a8a865ab
JacobG <jaqobek1995@gmail.com>
2021-08-17 01:24:02 +0200

    fixed css/html of pages + scroll into js

--- app/static/js/login.js
+++ app/static/js/login.js
@@ -1,1 +1,22 @@
-console.log('hello')
+/* Animations */
+
+const register_container = document.querySelector('#register-container');
+const show_register_button = document.querySelector('#option-register');
+const navbar = document.querySelector('#navbar');
+
+show_register_button.addEventListener('click', showRegister);
+
+function showRegister() {
+    register_container.className += ' show';
+    register_container.scrollIntoView({behavior: "smooth"})
+}
+
+window.addEventListener('scroll', () => {
+    if (document.body.scrollTop > 875 || document.documentElement.scrollTop > 875 ) {
+        navbar.classList.add("inverted");
+    }
+    else {
+        navbar.classList.remove("inverted");
+    }
+})
+
