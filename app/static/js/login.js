472d482f160d2e46579138c8be445c649501300d
JacobG <jaqobek1995@gmail.com>
2021-08-17 02:57:27 +0200

    final touches on display forms and scrolling

--- app/static/js/login.js
+++ app/static/js/login.js
@@ -3,7 +3,7 @@ const login_container = document.querySelector('#login-container');
 const register_container = document.querySelector('#register-container');
 const show_register_button = document.querySelector('#option-register');
 const show_login_button = document.querySelector('#option-login');
-const navbar = document.querySelector('#navbar');
+const navbar = document.querySelector('#navbar-login');
 
 show_register_button.addEventListener('click', showRegister);
 
@@ -16,21 +16,13 @@ show_login_button.addEventListener('click', showLogin);
 
 function showLogin() {
     login_container.classList.remove('show');
-    login_container.scrollIntoView({behavior: "smooth"})
+    navbar.scrollIntoView({behavior: "smooth"})
 }
 
-
 window.addEventListener('scroll', () => {
-    if (document.body.scrollTop > 875 || document.documentElement.scrollTop > 875 ) {
-        navbar.classList.add("inverted");
-    } else {
-        navbar.classList.remove("inverted");
-    }
-
     if (document.documentElement.scrollTop == 0) {
         if (register_container.classList.contains('show')) {
             register_container.classList.remove('show');
-            console.log('yo')
         }
     }
 })
