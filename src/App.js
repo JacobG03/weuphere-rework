4ae6982ea42a6cacd419e14b91ad41460c042a5a
JacobG03 <jacobgluszek03@gmail.com>
2021-09-08 19:45:33 +0200

    components structure added

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
 import React, { useState, useEffect } from 'react';
-import './App.css';
+import Navbar from './components/Navbar'
+import style from './app.module.css';
+
 
 function App() {
-  const [currentTime, setCurrentTime] = useState(0);
-
-  useEffect(() => {
-    fetch('/time').then(res => res.json()).then(data => {
-      setCurrentTime(data.time);
-    });
-  }, []);
-
   return (
-    <div className="App">
-      <header className="App-header">
-
-        ... no changes in this part ...
-
-        <p>The current time is {currentTime}.</p>
-      </header>
+    <div className={style.container}>
+      <Navbar />
     </div>
   );
 }
---
+++ src/components/Navbar.js
@@ -0,0 +1,13 @@
+import React, { useState } from 'react';
+import styles from './navbar.module.css'
+
+
+function Navbar() {
+  return (
+    <div className={styles.navbar}>
+      Navbar
+    </div>
+  )
+}
+
+export default Navbar;
