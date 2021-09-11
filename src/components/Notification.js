e7841d8b67ec016a8fba3b4ad56d18fa0e5db33b
JacobG03 <jacobgluszek03@gmail.com>
2021-09-11 10:52:22 +0200

    base for notification component

---
+++ src/components/Notification.js
@@ -0,0 +1,15 @@
+import React, { useState } from 'react';
+import { Link } from "react-router-dom";
+import styles from './notification.module.css'
+import {Animated} from "react-animated-css";
+
+
+function Notification(props) {
+  return (
+    <div className={styles['notification-box']}>
+      
+    </div>
+  )
+}
+
+export default Notification;
