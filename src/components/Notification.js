ba37b474e20d8b13f98810743dec56a6c7041fb8
JacobG03 <jacobgluszek03@gmail.com>
2021-09-14 19:07:59 +0200

    notification dleletion on click

--- src/components/Notification.js
+++ src/components/Notification.js
@@ -2,8 +2,10 @@ import React, { useState } from 'react';
 import styles from './notifications.module.css'
 import {Animated} from "react-animated-css";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { } from '@fortawesome/free-solid-svg-icons'
-
+import { 
+  faTimes
+} from '@fortawesome/free-solid-svg-icons'
+import { motion } from 'framer-motion';
 
 function Notification(props) {
   const [display, setDisplay] = useState(true)
@@ -31,7 +33,27 @@ function Notification(props) {
         isVisible={display}
       >
         <div className={styles['notification']}>
-          <span>{props.message}</span>
+          <span className={styles.message}>{props.message}</span>
+          <motion.div
+            className={styles.close}
+            whileHover={{ 
+              scale: 1.1,
+              cursor: 'pointer'
+            }}
+            whileTap={{ scale: 0.8 }}
+            onClick={() => {
+              setDisplay(false)
+              setTimeout(() => {
+                setDisplay(null)
+              }, 1000)
+            }}
+          >
+            <FontAwesomeIcon 
+              icon={faTimes}
+              size={'lg'}
+            >
+            </FontAwesomeIcon>
+          </motion.div>
           <div className={styles['loading']} />
         </div>
       </Animated>
