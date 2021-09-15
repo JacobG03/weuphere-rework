08e08870442dbf4799619ca8d5c78764e8e5a0c4
JacobG03 <jacobgluszek03@gmail.com>
2021-09-15 11:01:21 +0200

    register form component added

--- src/pages/LoginPage.js
+++ src/pages/LoginPage.js
@@ -52,18 +52,90 @@ function Register(props) {
       animationIn='slideInRight'
       animationOut='slideOutRight'
     >
-      <div 
-        className={styles['register']}
-        onClick={() => {
-          props.setDisplay(!props.display)
-        }}
-      >
-        Register Here
+      <div className={styles['register']}>
+        <RegisterForm
+          display={props.display}
+          setDisplay={props.setDisplay}
+        />
       </div>
     </Animated>
   )
 }
 
+function RegisterForm(props) {
+  const { register, handleSubmit, formState: { errors } } = useForm();
+  const onSubmit = data => console.log(data);
+  
+  return (
+    <form
+      className={styles['login-form']}
+      onSubmit={handleSubmit(onSubmit)}
+    >
+      <span className={styles['header']}>Register</span>
+
+      <span className={styles['title']}>Username:</span>
+      <input {...register('username', { required: true })} />
+      {errors.username &&
+      <Animated animationIn='fadeIn'>
+        <span className={styles['error']}>
+          This field is required
+        </span>
+      </Animated>
+      }
+
+      <span className={styles['title']}>Email:</span>
+      <input {...register('email', { required: true })} />
+      {errors.email &&
+      <Animated animationIn='fadeIn'>
+        <span className={styles['error']}>
+          This field is required
+        </span>
+      </Animated>
+      }
+      
+      <span className={styles['title']}>Password:</span>
+      <input type='password' {...register('password', { required: true })} />
+      {errors.password &&
+      <Animated animationIn='fadeIn'>
+        <span className={styles['error']}
+        >
+          This field is required
+        </span>
+      </Animated>
+      }
+
+      <span className={styles['title']}>Repeat Password:</span>
+      <input type='password' {...register('password2', { required: true })} />
+      {errors.password2 &&
+      <Animated animationIn='fadeIn'>
+        <span className={styles['error']}
+        >
+          This field is required
+        </span>
+      </Animated>
+      }
+
+      <motion.input
+        className={styles['btn']}
+        type="submit"
+        value='Sign up'
+        whileHover={{ cursor: 'pointer' }}
+        whileTap={{ 
+          scale: 0.9,
+        }}
+      />
+      <span>Already a user?</span>
+      <span 
+        className={styles['redirect']}
+        onClick={() => {
+          props.setDisplay(false)
+        }}
+      >
+        Click here to sign in
+      </span>
+    </form>
+  )
+}
 
 function LoginForm(props) {
   const { register, handleSubmit, formState: { errors } } = useForm();
