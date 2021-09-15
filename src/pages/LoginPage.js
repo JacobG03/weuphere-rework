14fd9f942686e899715e29cbeecefd72eb47de7a
JacobG03 <jacobgluszek03@gmail.com>
2021-09-15 08:57:00 +0200

    login form styled

--- src/pages/LoginPage.js
+++ src/pages/LoginPage.js
@@ -3,8 +3,12 @@ import React, {
   useRef, 
   useEffect
 } from 'react';
+import { useForm } from 'react-hook-form'
 import styles from './loginPage.module.css'
 import { Animated } from 'react-animated-css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faTimes } from '@fortawesome/free-solid-svg-icons'
+import { motion } from 'framer-motion';
 
 
 function LoginPage(props) {
@@ -49,10 +53,55 @@ function LoginPage(props) {
 }
 
 function LoginForm(props) {
+  const { register, handleSubmit, watch, formState: { errors } } = useForm();
+  const onSubmit = data => console.log(data);
+  
   return (
-    <div className={styles['login-form']}>
+    <form
+      className={styles['login-form']}
+      onSubmit={handleSubmit(onSubmit)}
+    >
+      <span className={styles['header']}>Login</span>
 
-    </div>
+      <span className={styles['title']}>Email:</span>
+      <input {...register("email", { required: true })} />
+      {errors.email &&
+      <Animated animationIn='fadeIn'>
+        <span className={styles['error']}>
+          This field is required
+        </span>
+      </Animated>
+      }
+      
+      <span className={styles['title']}>Password:</span>
+      <input type='password' {...register("password", { required: true })} />
+      {errors.password &&
+      <Animated animationIn='fadeIn'>
+        <span className={styles['error']}
+        >
+          This field is required
+        </span>
+      </Animated>
+      }
+      <motion.input
+        className={styles['btn']}
+        type="submit"
+        value='Sign in'
+        whileHover={{ cursor: 'pointer' }}
+        whileTap={{ 
+          scale: 0.9,
+        }}
+      />
+      <span>New user?</span>
+      <span 
+        className={styles['redirect']}
+        onClick={() => {
+          props.setDisplayRegister(true)
+        }}
+      >
+        Click here to register
+      </span>
+    </form>
   )
 }
 
